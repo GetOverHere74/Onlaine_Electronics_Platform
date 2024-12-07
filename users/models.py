977c80d069e5481c5e0b0ab816e83217169b5b55
skyl1 <skyl1.maxla2@mail.ru>
2024-12-07 19:29:17 +0500

    Добавлена модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=20,
+        **NULLABLE,
+        verbose_name="Номер телефона",
+        help_text="Укажите номер телефона",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
