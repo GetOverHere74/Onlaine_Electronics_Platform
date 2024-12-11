a0f0aca8bdd302b93a5fddd90f364aed4f72bdc7
skyl1 <skyl1.maxla2@mail.ru>
2024-12-11 17:50:55 +0500

    CRUD для моделей

--- online_platform/views.py
+++ online_platform/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from online_platform.models import Product, Contact, Provider
+from online_platform.serializers import ProductSerializer, ContactSerializer, ProviderSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """CRUD для работы с продуктами"""
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ContactViewSer(viewsets.ModelViewSet):
+    """CRUD для работы с контактами"""
+
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    """CRUD для работы с поставщиками"""
+
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
