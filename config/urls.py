b0afbb8ab5910f4e5f6d8743484d9665c909b7b0
skyl1 <skyl1.maxla2@mail.ru>
2024-12-11 18:03:18 +0500

    Подключено приложение online_platform

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('online_platform.urls', namespace='online_platform')),
     path("users/", include("users.urls", namespace="users")),
 ]
