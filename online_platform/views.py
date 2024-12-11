45dd2ffcc03fb0c90a0caffa4094fc5808583f04
skyl1 <skyl1.maxla2@mail.ru>
2024-12-11 18:53:15 +0500

    Добавлены пермишены и фильтр

--- online_platform/views.py
+++ online_platform/views.py
@@ -1,20 +1,30 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from online_platform.models import Product, Contact, Provider
-from online_platform.serializers import ProductSerializer, ContactSerializer, ProviderSerializer
+from online_platform.permissions import IsActiveStaff
+from online_platform.serializers import (
+    ProductSerializer,
+    ContactSerializer,
+    ProviderSerializer,
+)
+from online_platform.services import CountryFilter
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     """CRUD для работы с продуктами"""
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsActiveStaff,)
 
 
-class ContactViewSer(viewsets.ModelViewSet):
+class ContactViewSet(viewsets.ModelViewSet):
     """CRUD для работы с контактами"""
 
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+    permission_classes = (IsActiveStaff,)
 
 
 class ProviderViewSet(viewsets.ModelViewSet):
@@ -22,3 +32,6 @@ class ProviderViewSet(viewsets.ModelViewSet):
 
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CountryFilter
+    permission_classes = (IsActiveStaff,)
